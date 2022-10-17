700627a97b2501fba4035c767311c18c2f4e799f
Simon Garnotel <simon.garnotel@gmail.com>
2022-10-17 09:45:08 +0200

    notarize

---
+++ build/notarize.js
@@ -0,0 +1,19 @@
+const { notarize } = require('electron-notarize')
+
+const notarizing = async (context) => {
+  const { electronPlatformName, appOutDir } = context
+  if (electronPlatformName !== 'darwin') {
+    return
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  return notarize({
+    appBundleId: 'com.tanatloc.app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD
+  })
+}
+
+exports.default = notarizing
