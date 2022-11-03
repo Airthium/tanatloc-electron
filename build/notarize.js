25b4b7480334719fd196d3912136e6be810a830d
Simon Garnotel <simon.garnotel@gmail.com>
2022-11-03 15:51:45 +0100

    fix

--- build/notarize.js
+++ build/notarize.js
@@ -1,4 +1,4 @@
-const { notarize } = require('electron-notarize')
+const { notarize } = require('@electron/notarize')
 
 const notarizing = async (context) => {
   const { electronPlatformName, appOutDir } = context
