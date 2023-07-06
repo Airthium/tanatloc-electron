39845ccb9061e1c128743c414ae9755646362016
Simon Garnotel <simon.garnotel@gmail.com>
2023-07-06 11:57:16 +0200

    notarize

--- build/notarize.js
+++ build/notarize.js
@@ -15,7 +15,7 @@ const notarizing = async (context) => {
   return notarize({
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID
   })
 }
